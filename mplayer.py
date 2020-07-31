5ea026d669afc89f258bf0a66039c9afe96bb987
unknown <thaivanphu.qn95@gmail.com>
2020-07-31 23:36:46 +0700

    button image 2

--- mplayer.py
+++ mplayer.py
@@ -7,18 +7,25 @@ root.geometry("1024x768")
 
 # Create Playlist Box
 playlist_box = Listbox(root, bg="#00b894", fg="#dfe6e9", width=100)
-playlist_box.pack(pady=50)
+playlist_box.pack(pady=20)
+
+# Define Button Image For Controls
+back_button_img = PhotoImage(file='images/back50.png')
+forward_button_img = PhotoImage(file='images/forward50.png')
+play_button_img = PhotoImage(file='images/play50.png')
+pause_button_img = PhotoImage(file='images/pause50.png')
+stop_button_img = PhotoImage(file='images/stop50.png')
 
 #Create Button Frame
 control_frame = Frame(root)
 control_frame.pack(pady=20)
 
 #Create Play/Stop etc Buttons
-back_button = Button(control_frame, text="Back")
-forward_button = Button(control_frame, text="Forward")
-play_button = Button(control_frame, text="Play")
-pause_button = Button(control_frame, text="Pause")
-stop_button = Button(control_frame, text="Stop")
+back_button = Button(control_frame, image=back_button_img, borderwidth=0)
+forward_button = Button(control_frame, image=forward_button_img, borderwidth=0)
+play_button = Button(control_frame, image=play_button_img, borderwidth=0)
+pause_button = Button(control_frame, image=pause_button_img, borderwidth=0)
+stop_button = Button(control_frame, image=stop_button_img, borderwidth=0)
 
 back_button.grid(row=0, column=0, padx=10)
 forward_button.grid(row=0, column=1, padx=10)
